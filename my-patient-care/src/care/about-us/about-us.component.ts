3096436e66f3ea0494a8bca3b8e25b1781d77dbf
Darko Tacic <darko.tacic@outlook.com>
2019-08-03 17:25:13 +0200

    Implemened google maps on the UI

--- my-patient-care/src/care/about-us/about-us.component.ts
+++ my-patient-care/src/care/about-us/about-us.component.ts
@@ -1,6 +1,12 @@
 import { HospitalService } from './shared/hospital.service';
-import { OnInit, Component } from "@angular/core";
+import { OnInit, Component, ViewChild } from "@angular/core";
 import { Hospital } from './shared/hospital.model';
+import { registerElement } from 'nativescript-angular/element-registry';
+import { MapView, Marker, Position } from 'nativescript-google-maps-sdk';
+
+
+// Important - must register MapView plugin in order to use in Angular templates
+registerElement('MapView', () => MapView);
 
 @Component({
     selector: "AboutUs",
@@ -13,6 +19,18 @@ export class AboutUsComponent implements OnInit {
 
     hospital: Hospital;
     isLoaded: boolean;
+
+    latitude =  -33.86;
+    longitude = 151.20;
+    zoom = 8;
+    minZoom = 0;
+    maxZoom = 22;
+    bearing = 0;
+    tilt = 0;
+    padding = [40, 40, 40, 40];
+    mapView: MapView;
+
+    lastCamera: String;
 
     constructor(private hospitalService : HospitalService) {
         
@@ -30,4 +48,39 @@ export class AboutUsComponent implements OnInit {
             }
         );
     }
+
+        //Map events
+        onMapReady(event) {
+            console.log('Map Ready');
+    
+            this.mapView = event.object;
+    
+            console.log("Setting a marker...");
+    
+            var marker = new Marker();
+            marker.position = Position.positionFromLatLng(-33.86, 151.20);
+            marker.title = "Sydney";
+            marker.snippet = "Australia";
+            marker.userData = {index: 1};
+            this.mapView.addMarker(marker);
+        }
+    
+        onCoordinateTapped(args) {
+            console.log("Coordinate Tapped, Lat: " + args.position.latitude + ", Lon: " + args.position.longitude, args);
+        }
+    
+        onMarkerEvent(args) {
+            console.log("Marker Event: '" + args.eventName
+                + "' triggered on: " + args.marker.title
+                + ", Lat: " + args.marker.position.latitude + ", Lon: " + args.marker.position.longitude, args);
+        }
+    
+        onCameraChanged(args) {
+            console.log("Camera changed: " + JSON.stringify(args.camera), JSON.stringify(args.camera) === this.lastCamera);
+            this.lastCamera = JSON.stringify(args.camera);
+        }
+    
+        onCameraMove(args) {
+            console.log("Camera moving: " + JSON.stringify(args.camera));
+        }
 }
